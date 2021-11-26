0214c1a56960a5266e0b590789f6e19d7ba4f2b0
Munavvarhushen Durvesh <munavvard222@gmail.com>
2021-11-26 16:41:57 +0530

    multi tile

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 jQuery.fn.tagName = function() {
     return this.prop("tagName").toLowerCase();
   };
-const position = { x: 0, y: 0 }
+// let position = { x: 0, y: 0 };
+            
 jQuery(document).ready(function($){
     interact('.draggable').draggable({
         listeners: {
             start (event) {
+                // position = { x: 0, y: 0 };
+                // let bro = event.target.getBoundingClientRect();
+                // position.x = bro.x;
+                // position.y = bro.y;
             },
             move (event) {
-            position.x += event.dx;position.y += event.dy;
+                console.log(event);
             event.target.style.transform =
-                `translate(${position.x}px, ${position.y}px)`
-            },
+                `translate(${event.page.x}px, ${event.page.y}px)`
+            }
         }
     });
     generate();
@@ -34,11 +39,11 @@ let getParsedStyle = (element)=>{
     return element.attr('style').split(';').map((s)=>{ return s.trim().split(':'); });
 };
 let getParsedPosition = (position)=>{
-    let x = (position.x * 100) / 600;
-    let y = (position.y * 100) / 400;
+    let x = (position.x * 100) / 1280;
+    let y = (position.y * 100) / 640;
     
-    let w = (position.width * 100) / 600;
-    let h = (position.height * 100) / 400;
+    let w = (position.width * 100) / 1280;
+    let h = (position.height * 100) / 640;
     
     return {x,y,w,h};
 };
@@ -46,7 +51,7 @@ let getPosition = (element)=>{
     return element.get(0).getBoundingClientRect();
 };
 let getJSONConfigByTag = (element)=>{
-     style = getParsedStyle(element); 
+    let style = getParsedStyle(element); 
     let position = getPosition(element);
     let parsedPosition = getParsedPosition(position);
     let colorStyle = style.filter((s)=>{ return s[0] == "background-color"; });
@@ -70,8 +75,8 @@ let getJSONConfigByTag = (element)=>{
         textAlign = textAlignStyle[0][1];
     }
     if(roundedStyle.length > 0){
-        rectRadius = (parseInt(roundedStyle[0][1].replace('px','').replace('%','')) * 100) / 180;
-        console.log(rectRadius);
+        rectRadius = Math.round((parseInt(roundedStyle[0][1].replace('px','').replace('%','')) * 100) / 180,1) / 20;
+        // rectRadius = 0;
     }
     switch(element.tagName()){
         case 'div':
@@ -92,20 +97,32 @@ let getJSONConfigByTag = (element)=>{
             };
             break;
         case 'img':
-            return [];
+            return {
+                'IMAGE':{
+                    x: parsedPosition.x+'%',
+                    y: parsedPosition.y+'%',
+                    w: parsedPosition.w+'%',
+                    h: parsedPosition.h+'%',
+                    path:element.attr('src')
+                }
+            };
             break;
         default:
-            return [];
+            return {};
             break;
     }
 };
 let parseHtml = (holder,jsonConfig)=>{
-    if(holder.children().length != 0){
-        return parseHtml(holder.children(),jsonConfig);
-    }else{
-        jsonConfig.push(getJSONConfigByTag(holder));
-        return jsonConfig;
-    }
+    // if(holder.children().length != 0){
+    //     return parseHtml(holder.children(),jsonConfig);
+    // }else{
+    //     jsonConfig.push(getJSONConfigByTag(holder));
+    //     return jsonConfig;
+    // }
+    holder.each((id,elem)=>{
+        jsonConfig.push(getJSONConfigByTag($(elem)));
+    });
+    return jsonConfig;
 };
 
 let generate = function() {
@@ -114,15 +131,25 @@ let generate = function() {
     let slide = pptx.addSlide('Jay Bhavsar'); 
     
     let jsonConfig = parseHtml($('.holder').children(),[]);
+    console.log(jsonConfig);
     $.each(jsonConfig,(type,config)=>{
         let configId = Object.keys(config)[0];
         let configElement = config[Object.keys(config)[0]];
-        if('text' in configElement){
-            slide.addText(configElement.text,configElement);
+        switch (configId){
+            case 'RECTANGLE':
+                if('text' in configElement){
+                    slide.addText(configElement.text,configElement);
+                }
+                else{
+                    slide.addShape(pptx.shapes[configId], configElement);         
+                }
+                break;
+            case 'IMAGE':
+                slide.addImage(configElement);
+                break;
+                
         }
-        else{
-            slide.addShape(pptx.shapes[configId], configElement);         
-        }
+        
     });
 
     pptx.writeFile({
